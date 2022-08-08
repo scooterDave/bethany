361a9d5216db988c9b6e69700ac5ab8869ef4c96
david@digitalguerrilla.com <david@digitalguerrilla.com>
2022-08-08 14:59:48 -0400

    updated title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@ require("dotenv").config({
 })
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Bethany Presbyterian`,
+    description: `Site under development`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
